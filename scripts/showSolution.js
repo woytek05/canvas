b1141402adc75b5224ffdb072b36196b3cae2822
woytek05 <woytek05@gmail.com>
2022-04-30 14:06:59 +0200

    Finish page about lines

---
+++ scripts/showSolution.js
@@ -0,0 +1,13 @@
+const button = document.querySelector(".show-solution");
+const codeContainer = document.querySelector(".code-container.exercise");
+
+button.addEventListener("click", () => {
+    codeContainer.classList.toggle("exercise");
+    if (button.classList.contains("default-color")) {
+        button.classList.remove("default-color");
+        button.classList.add("primary-color");
+    } else {
+        button.classList.remove("primary-color");
+        button.classList.add("default-color");
+    }
+})
--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,7 +1,6 @@
 const body = document.querySelector("body");
 const sidebar = body.querySelector(".sidebar");
 const toggle = body.querySelector(".toggle");
-const txt = body.querySelector(".text")
 
 toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
